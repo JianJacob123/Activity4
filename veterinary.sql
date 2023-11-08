293d8a88dff6a1c0e0bd0022e42112907b86e2e9
Joshua-Almario7 <joshuaalmario10@gmail.com>
2023-11-08 09:29:22 +0800

    feat create table animals

--- veterinary.sql
+++ veterinary.sql
@@ -7,3 +7,16 @@ CREATE TABLE owners(ownerid INT NOT NULL PRIMARY KEY,
 				   address VARCHAR(50) NOT NULL,
 				   phone VARCHAR(15) NOT NULL,
 				   email VARCHAR(100) NOT NULL);
+
+--CREATE TABLES ANIMALS
+create table animals(
+animalid int primary key,
+name varchar (50),
+species varchar(50),
+breed varchar(50),
+dateofbirth date,
+gender varchar(10),
+color varchar(50),
+ownerid int not null,
+foreign key (ownerid) references owners (ownerid)
+);
