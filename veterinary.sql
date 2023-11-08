ce41696172403a74bfb507516a26528351018ccb
JianJacob123 <larinojianjacob@gmail.com>
2023-11-08 09:19:09 +0800

    feat added table owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,9 @@
 Create Database Veterinary;
+
+--CREATE TABLES
+CREATE TABLE owners(ownerid INT NOT NULL PRIMARY KEY,
+				   ofirstname VARCHAR(50) NOT NULL,
+				   olastname VARCHAR(50) NOT NULL,
+				   address VARCHAR(50) NOT NULL,
+				   phone VARCHAR(15) NOT NULL,
+				   email VARCHAR(100) NOT NULL);
